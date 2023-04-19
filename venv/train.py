eb9709b85a0938d451069a7f4f7825cf58ccc571
YaroslavYushkevych <yaroslavyushkevych@gmail.com>
2023-04-19 12:26:23 +0300

    Add_venv_pack

---
+++ venv/train.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+layer_weights = np.array([])
+layer_biases = np.array([])
+
+layer_weights = np.append(layer_weights, int(1))
+layer_biases = np.append(layer_biases, '0')
+
+print(layer_weights)
+print(layer_biases)
